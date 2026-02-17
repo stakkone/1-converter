de8ac11bb8f15172e35c6fc8f58455695e2a8c54
stakkone <arseqpage@gmail.com>
2026-02-17 20:27:51 +0300

    2 user input

--- main.go
+++ main.go
@@ -2,7 +2,24 @@ package main
 
 import "fmt"
 
+func getUserInput() float64 {
+	var userHeight float64
+	fmt.Print("Введите данные: ")
+	fmt.Scanln(&userHeight)
+
+	return userHeight
+}
+
+func convert(
+	number float64,
+	currentValue string,
+	targetValue string,
+) {
+
+}
+
 func main() {
+	var userInput = getUserInput()
 	const usdToEur float64 = 0.8429
 	const usdToRub float64 = 76.52
 
