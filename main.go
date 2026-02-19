e84f05c619933c029dfc2b3ca61d95a5894d8386
stakkone <arseqpage@gmail.com>
2026-02-19 13:51:55 +0300

    3 convert

--- main.go
+++ main.go
@@ -1,29 +1,116 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
-func getUserInput() float64 {
-	var userHeight float64
-	fmt.Print("Введите данные: ")
-	fmt.Scanln(&userHeight)
+/*
+Сделать меню с шагами
+- ввод исходно валюты (подсказывать варианты, если ошибка, заново вводит
+- ввод числа, если ошибка, заново вводит
+- ввод целевой валюты, если ошибка, заново вводит
+*/
 
-	return userHeight
+func getUserStringInput(valuteText string) string {
+	var inputValue string
+	var inputText string = fmt.Sprintf("Введите %s валюту (rub / eur / usd): ", valuteText)
+
+	for {
+		fmt.Printf(inputText)
+
+		var _, err = fmt.Scanln(&inputValue)
+		inputValue = strings.TrimSpace(inputValue)
+		fmt.Println("err", err)
+
+		if err != nil || inputValue != "rub" && inputValue != "eur" && inputValue != "usd" {
+			fmt.Println("Введены некорректные данные")
+
+			continue
+		}
+
+		return inputValue
+	}
+}
+
+func getUserNumberInput() float64 {
+	var inputValue float64
+	var inputText string = "Введите число с точкой: "
+
+	for {
+		fmt.Printf(inputText)
+		var _, err = fmt.Scanln(&inputValue)
+
+		if err != nil {
+			var dummy string
+			fmt.Scanln(&dummy)
+
+			fmt.Println("Введены некорректные данные")
+
+			continue
+		}
+
+		return inputValue
+	}
 }
 
 func convert(
 	number float64,
 	currentValue string,
 	targetValue string,
-) {
+) float64 {
+	const usdToEur float64 = 0.85
+	const usdToRub float64 = 76.65
+	const eurToRub = usdToRub / usdToEur // 90.18
+	var converterValue float64
 
+	if currentValue == targetValue {
+		return number
+	}
+
+	switch {
+	case currentValue == "rub":
+		if targetValue == "eur" {
+			converterValue = usdToEur / usdToRub
+		}
+
+		if targetValue == "usd" {
+			converterValue = usdToEur / usdToRub
+		}
+
+	case currentValue == "usd":
+		if targetValue == "eur" {
+			converterValue = usdToEur
+		}
+
+		if targetValue == "rub" {
+			converterValue = usdToRub
+		}
+
+	case currentValue == "eur":
+		if targetValue == "usd" {
+			converterValue = eurToRub / usdToRub
+		}
+
+		if targetValue == "rub" {
+			converterValue = eurToRub
+		}
+	}
+
+	return converterValue * number
 }
 
 func main() {
-	var userInput = getUserInput()
-	const usdToEur float64 = 0.8429
-	const usdToRub float64 = 76.52
+	var numberValue = getUserNumberInput()
+	fmt.Println("numberValue", numberValue)
 
-	const eurToRub = usdToRub / usdToEur
+	var currentValue = getUserStringInput("исходную")
+	fmt.Println("currentValue", currentValue)
 
-	fmt.Println("eurToRub", eurToRub)
+	var targetValue = getUserStringInput("целевую")
+	fmt.Println("targetValue", targetValue)
+
+	var convertValue = convert(numberValue, currentValue, targetValue)
+
+	fmt.Println("convertValue", convertValue)
 }
